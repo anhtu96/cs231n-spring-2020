ede4586531b980cf2752a900a754c1bdfde049ad
anhtu96 <tungo.ee.96@gmail.com>
2020-07-04 09:20:50 +0700

    complete StyleTransfer-TF

--- assignment3/cs231n/style_transfer_tensorflow.py
+++ assignment3/cs231n/style_transfer_tensorflow.py
@@ -15,8 +15,10 @@ def tv_loss(img, tv_weight):
     """
     # Your implementation should be vectorized and not require any loops!
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    first_sum = tf.reduce_sum(tf.square(img[:,1:,:,:] - img[:,:-1,:,:]))
+    second_sum = tf.reduce_sum(tf.square(img[:,:,1:,:] - img[:,:,:-1,:]))
+    loss = tv_weight * (first_sum + second_sum)
+    return loss
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -41,8 +43,13 @@ def style_loss(feats, style_layers, style_targets, style_weights):
     # Hint: you can do this with one for loop over the style layers, and should
     # not be short code (~5 lines). You will need to use your gram_matrix function.
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    loss = 0
+    for i in range(len(style_layers)):
+        layer = style_layers[i]
+        G = gram_matrix(feats[layer])
+        A = gram_matrix(style_targets[i])
+        loss += style_weights[i] * tf.reduce_sum(tf.square(G - A))
+    return loss
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -61,8 +68,11 @@ def gram_matrix(features, normalize=True):
       Gram matrices for the input image.
     """
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    features = tf.reshape(features, (-1, features.shape[-1]))
+    gram = tf.linalg.matmul(tf.transpose(features), features)
+    if normalize:
+        gram = gram / (features.shape[0] * features.shape[1])
+    return gram
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -79,9 +89,8 @@ def content_loss(content_weight, content_current, content_original):
     - scalar content loss
     """
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
-
+    content_loss = content_weight * tf.math.reduce_sum((content_current - content_original)**2)
+    return content_loss
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
 # We provide this helper code which takes an image, a model (cnn), and returns a list of
